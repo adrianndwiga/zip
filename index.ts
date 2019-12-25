3096253f1161fa5d5c0c8cd6a482b368752558ff
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-12-25 18:48:02 +0100

    removing redundant code from index.ts

--- index.ts
+++ index.ts
@@ -1,36 +1,25 @@
-import { createReadStream, createWriteStream, readFileSync, writeFileSync, unlinkSync, existsSync } from 'fs'
-import { createGzip, createGunzip } from 'zlib'
-import { randomBytes, createCipheriv, createDecipheriv } from 'crypto';
-import { Config } from './types';
+import { readdirSync, lstatSync, Stats } from 'fs'
 
-export function zip (source: string, target: string) {
-    const gzip = createGzip();
-    const inp = createReadStream(source)
-    const out = createWriteStream(target)
-    inp.pipe(gzip).pipe(out)    
+export function _zipFolder(fullPath: string, folder: string) {
+    const _dir = readdirSync(fullPath)
+
+    const func = (name: string, stats: Stats) => {
+        console.log(`${fullPath}/${name} is a directory ${stats.isDirectory()}`)
+    }
+
+    _dir.forEach(dir => func(dir, lstatSync(dir)))
 }
 
-export function unzip(source: string, target: string) {
-    const inp = createReadStream(source)
-    const out = createWriteStream(target)
-    const unzip = createGunzip()
-    inp.pipe(unzip).pipe(out)
+export function zipFolder(folderName: string, source: string) {
+    const dirs = readdirSync(folderName)
+    const func = (dir: string) => {
+        if(dir !== '.DS_Store' && lstatSync(dir).isDirectory())
+            _zipFolder(folderName, dir)
+    }
+
+    dirs.forEach((dir) => 
+        func(dir)
+    )
 }
 
-// export function encrypt(config: Config, key: Buffer, iv: Buffer, source: string, target: string) {
-//     const content = readFileSync(source, 'utf8')
-//     let cipher = createCipheriv(config.algorithm, Buffer.from(key), iv)
-//     let encrypted = cipher.update(content)
-//     encrypted = Buffer.concat([encrypted, cipher.final()]);
-    
-//     writeFileSync(target, encrypted.toString(config.encoding), 'utf8')    
-// }
-
-// export function decrypt(config: Config, key: Buffer, iv: Buffer, source: string, target: string): void {
-//     const encrypted = Buffer.from(readFileSync(source, 'utf8'), config.encoding)
-//     const decipher = createDecipheriv(config.algorithm, Buffer.from(key), iv)
-//     let decrypted = decipher.update(encrypted)
-//     decrypted = Buffer.concat([decrypted, decipher.final()])
-
-//     writeFileSync(target, decrypted.toString(), 'utf8')
-// }
+zipFolder('../_stores/', '_stores.zip')
