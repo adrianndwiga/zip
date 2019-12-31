12a3ef502588a45c65475989752e2efe32469e88
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-12-31 14:46:32 +0100

    adding zip / unzip stream

--- zip.ts
+++ zip.ts
@@ -1,4 +1,4 @@
-import { createReadStream, createWriteStream } from 'fs'
+import { createReadStream, createWriteStream, ReadStream, WriteStream } from 'fs'
 import { createGzip, createGunzip } from 'zlib'
 
 export function zip (source: string, target: string) {
@@ -8,9 +8,19 @@ export function zip (source: string, target: string) {
     inp.pipe(gzip).pipe(out)    
 }
 
+export function zipStream (inp: ReadStream, out: WriteStream) {
+    const gzip = createGzip();
+    inp.pipe(gzip).pipe(out)    
+}
+
 export function unzip(source: string, target: string) {
     const inp = createReadStream(source)
     const out = createWriteStream(target)
+    const unzip = createGunzip()
+    inp.pipe(unzip).pipe(out)
+}
+
+export function unzipStream(inp: ReadStream, out: WriteStream) {
     const unzip = createGunzip()
     inp.pipe(unzip).pipe(out)
 }
